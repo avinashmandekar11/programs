b43a8b70c1b1914a3bf1d27f91891f5552a25537
avinashmandekar <avinash322003m@gmail.com>
2025-03-27 09:02:45 +0530

    sample changes

--- Testing shastra/java/practise set/P1/divisible25.java
+++ Testing shastra/java/practise set/P1/divisible25.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class divisible25 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a no");
+        System.out.println("Enter a no5");
         int no  =sc.nextInt();
         if(no%3==0 && no%5==0){
             System.out.println(no+" is divisible by 3 and 5 both");
---
+++ Testing shastra/java/practise set/P1/pprime.java
@@ -0,0 +1,16 @@
+public class pprime {
+
+    public static boolean isPrime(int n){
+        for(int i=2;i<n;i++){
+            if(i%n==0){
+            return false;
+            }
+        }
+             return true;
+    }
+
+
+    public static void main(String[] args) {
+       System.out.println(isPrime(5)); 
+    }
+}
